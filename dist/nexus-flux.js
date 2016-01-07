da52da085bf5364f7d6f3bb4d8bd33578f57b7f7
FranzPoize <francois.poizat@gmail.com>
2016-01-07 11:40:44 +0100

    add dist because i'm a lib

---
+++ dist/nexus-flux.js
@@ -0,0 +1,11 @@
+import Client from './Client';
+import Lifespan from 'lifespan';
+import Remutable from 'remutable';
+import Server from './Server';
+
+export default {
+  Client,
+  Lifespan,
+  Remutable,
+  Server
+};
